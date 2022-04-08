03cf004cc6fc10017f9df022b8a47a59153b0a47
Keyou <lkysybil@gmail.com>
2022-04-08 15:16:38 +0800

    fixed minor syntax error at views.py

--- website/views.py
+++ website/views.py
@@ -191,7 +191,11 @@ def settings_success(request):
             interest3 = (lambda x: "interest_3 = '" + interest3 + "'"if len(x)!=0 else "")(interest3)
             interest4 = (lambda x: "interest_4 = '" + interest4 + "'"if len(x)!=0 else "")(interest4)
             interest5 = (lambda x: "interest_5 = '" + interest5 + "'"if len(x)!=0 else "")(interest5)
-            parameters = ','.join([update_height, update_education, update_rate, interest1, interest2, interest3, interest4, interest5])
+            paramlist = []
+            for i in [update_height, update_education, update_rate, interest1, interest2, interest3, interest4, interest5]:
+                if len(i)!=0:
+                    paramlist.append(i)
+            parameters = ','.join(paramlist)
             if len(parameters)>0:
                 c.execute("UPDATE buddies SET "+parameters+" WHERE your_email = '"+email+"';")
     return render(request, 'settings_success.html')
